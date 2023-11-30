307697bc742f53f9ccdb8d8f1fe9b7b76254dda5
germoga11 <78922181+germoga11@users.noreply.github.com>
2023-11-29 21:03:38 -0600

    Update Listas.h

--- Listas.h
+++ Listas.h
@@ -111,6 +111,83 @@ public:
         }
         delete[] clientes;
     }
+
+//HABITACIONES
+
 };
+class Habitacion {
+private:
+    int numero;
+    bool disponible;
+    string ocupante;
+
+public:
+    Habitacion(int num) : numero(num), disponible(true), ocupante("") {}
+
+    void MostrarHabitacion() {
+        cout << "\n------------------------------------------------------";
+        cout << "\n\tNúmero de Habitación: " << numero;
+        cout << "\n\tEstado: " << (disponible ? "Disponible" : "No Disponible");
+        if (!disponible) {
+            cout << "\n\tOcupante: " << ocupante;
+        }
+        cout << "\n------------------------------------------------------";
+    }
+
+    void CambiarDisponibilidad() {
+        disponible = !disponible;
+        if (!disponible) {
+            cout << "\nIngrese el nombre del ocupante: ";
+            cin.ignore(); // Limpiar el buffer
+            getline(cin, ocupante);
+        } else {
+            ocupante = ""; // Limpiar el nombre del ocupante si la habitación está disponible
+        }
+    }
+
+    bool EstaDisponible() const {
+        return disponible;
+    }
+};
+
+class ListaHabitaciones {
+private:
+    Habitacion* habitaciones[10];
+
+public:
+    ListaHabitaciones() {
+        for (int i = 0; i < 10; ++i) {
+            habitaciones[i] = new Habitacion(i + 1);
+        }
+    }
+
+    ~ListaHabitaciones() {
+        for (int i = 0; i < 10; ++i) {
+            delete habitaciones[i];
+        }
+    }
+
+    void MostrarLista() {
+        cout << "\n\tLista de Habitaciones:\n";
+        for (int i = 0; i < 10; ++i) {
+            habitaciones[i]->MostrarHabitacion();
+            cout << endl;
+        }
+    }
+
+    void CambiarEstadoHabitacion() {
+        int numHabitacion;
+        cout << "\nIngrese el número de la habitación que desea cambiar (1-10): ";
+        cin >> numHabitacion;
+
+        if (numHabitacion >= 1 && numHabitacion <= 10) {
+            habitaciones[numHabitacion - 1]->CambiarDisponibilidad();
+            cout << "\nEstado de la habitación cambiado exitosamente.\n";
+        } else {
+            cout << "\nNúmero de habitación inválido.\n";
+        }
+    }
+};
+
 
 #endif
